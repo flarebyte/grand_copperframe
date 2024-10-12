ab8ff3ac8f7e54916925b2e76853667c278547d2
Olivier Huin <olih@users.noreply.github.com>
2024-10-12 20:01:33 +0100

    Adds copperframe code

--- example/example.dart
+++ example/example.dart
@@ -1,6 +1,25 @@
 import 'package:grand_copperframe/grand_copperframe.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  final message = CopperframeMessage(
+    label: 'This is an info message',
+    level: CopperframeMessageLevel.info,
+    category: 'usage',
+  );
+  final rule = CopperframeFieldRule(
+    name: 'Length Check',
+    options: {'min': '1', 'max': '255'},
+    successMessages: [
+      CopperframeMessage(
+          label: 'Valid length',
+          level: CopperframeMessageLevel.info,
+          category: 'length')
+    ],
+    failureMessages: [
+      CopperframeMessage(
+          label: 'Invalid length',
+          level: CopperframeMessageLevel.error,
+          category: 'length')
+    ],
+  );
 }
