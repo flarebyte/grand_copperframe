54a995e0c9db1f2f533d48dfb7dab04ec09e7f00
Olivier Huin <olih@users.noreply.github.com>
2024-10-13 19:50:24 +0100

    Update example

--- example/example.dart
+++ example/example.dart
@@ -1,4 +1,29 @@
 import 'package:grand_copperframe/grand_copperframe.dart';
+import 'package:validomix/validomix.dart';
+
+class SimpleRule extends VxBaseRule<CopperframeMessage> {
+  @override
+  List<CopperframeMessage> validate(Map<String, String> options, String value) {
+    final message = CopperframeMessage(
+      label: 'Simple message',
+      level: CopperframeMessageLevel.info,
+      category: 'usage',
+    );
+    return [message];
+  }
+}
+
+class SecondRule extends VxBaseRule<CopperframeMessage> {
+  @override
+  List<CopperframeMessage> validate(Map<String, String> options, String value) {
+    final message = CopperframeMessage(
+      label: 'Second message',
+      level: CopperframeMessageLevel.info,
+      category: 'usage',
+    );
+    return [message];
+  }
+}
 
 void main() {
   final message = CopperframeMessage(
@@ -22,4 +47,11 @@ void main() {
           category: 'length')
     ],
   );
+
+  final rule1 =
+      CopperframeRule(rule: SimpleRule(), options: {'key113': 'value123'});
+  final rule2 =
+      CopperframeRule(rule: SecondRule(), options: {'key114': 'value125'});
+  final composer = CopperframeRuleComposer([rule1, rule2]);
+  composer.validate('some text');
 }
